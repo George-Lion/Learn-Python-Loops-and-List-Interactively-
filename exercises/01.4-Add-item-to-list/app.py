dc09d5754c0110202c0218d362505fa1ca63053f
Jorge Leon <jorgeleonpicture@gmail.com>
2022-04-25 18:02:37 +0000

    exercise update

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,17 @@
 #Remember import random function here:
+import random
 
 my_list = [4,5,734,43,45]
 
 #The magic is here:
+
+for i in range(10):
+    arr = random.randint(1, 500)
+    my_list.append(arr)
+    print(my_list)
+
+        
+
+
+
+
--- exercises/01.5-loop-seventeen/app.py
+++ exercises/01.5-loop-seventeen/app.py
@@ -1,1 +1,4 @@
 # Your code here, have fun:
+
+for value in range(1, 18):
+    print(value)
